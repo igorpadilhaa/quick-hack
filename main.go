48b8a41e4bdc151185f7a629066301d20fc66668
padilha <igor.padilha2004@gmail.com>
2023-07-20 23:00:33 -0300

    load entries to PATH by name

--- main.go
+++ main.go
@@ -6,6 +6,28 @@ import (
 	"strings"
 )
 
+var KnownApps = map[string]string {
+    "java": "path/to/jdk",
+    "node": "path/to/node",
+}
+
+func loadPaths(apps []string) []string {
+    var paths []string
+
+    for _, appName := range apps {
+        appPath, exists := KnownApps[appName]
+
+        if !exists {
+            fmt.Fprintf(os.Stderr, "Unknown app '%s'\n", appName)
+            continue
+        }
+
+        paths = append(paths, appPath)
+    }
+
+    return paths
+}
+
 func main() {
 	args := os.Args
 
@@ -14,9 +36,16 @@ func main() {
 	}
 
 	script := "export PATH=${PATH}"
-        
+        var newEntries []string
+
+        if args[1] == "add" {
+            newEntries = loadPaths(args[2:])
+        } else {
+            newEntries = args[1:]
+        }
+
         separator := string(os.PathListSeparator)
-        script += separator + strings.Join(args[1:], separator)
+        script += separator + strings.Join(newEntries, separator)
 
         fmt.Println(script)
 }
