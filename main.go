1d399a333165d717d862fa03cfd7aa5060940ab5
padilha <igor.padilha2004@gmail.com>
2023-07-23 21:11:20 -0300

    format code

--- main.go
+++ main.go
@@ -10,87 +10,88 @@ import (
 var KnownApps map[string]string
 
 func parseAppList(listJson []byte) map[string]string {
-    var appList map[string]string
+	var appList map[string]string
 
-    err := json.Unmarshal(listJson, &appList)
+	err := json.Unmarshal(listJson, &appList)
 
-    if err != nil {
-        fmt.Fprintf(os.Stderr,"Failed to parse app list: %s\n", err)
-    }
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to parse app list: %s\n", err)
+	}
 
-    return appList
+	return appList
 }
 
 func loadPaths(apps []string) []string {
-    var paths []string
+	var paths []string
 
-    for _, appName := range apps {
-        appPath, exists := KnownApps[appName]
+	for _, appName := range apps {
+		appPath, exists := KnownApps[appName]
 
-        if !exists {
-            fmt.Fprintf(os.Stderr, "Unknown app '%s'\n", appName)
-            continue
-        }
+		if !exists {
+			fmt.Fprintf(os.Stderr, "Unknown app '%s'\n", appName)
+			continue
+		}
 
-        paths = append(paths, appPath)
-    }
+		paths = append(paths, appPath)
+	}
 
-    return paths
+	return paths
 }
 
 func IsConfigValid() bool {
-    valid := true
+	valid := true
 
-    for appName, appPath := range KnownApps {
-        pathInfo, err := os.Stat(appPath)
+	for appName, appPath := range KnownApps {
+		pathInfo, err := os.Stat(appPath)
 
-        if err != nil {
-            valid = false
-            
-            fmt.Fprintf(os.Stderr, "Error on app '%s' (%s): %s\n", appName, appPath, err)
-            continue;
-        }
-        
-        if !pathInfo.IsDir() {
-            valid = false
-            fmt.Fprintf(os.Stderr, "Erro on app '%s': path must be a directory, got %s\n", appName, appPath)
-        }
-    }
+		if err != nil {
+			valid = false
 
-    return valid
+			fmt.Fprintf(os.Stderr, "Error on app '%s' (%s): %s\n", appName, appPath, err)
+			continue
+		}
+
+		if !pathInfo.IsDir() {
+			valid = false
+			fmt.Fprintf(os.Stderr, "Erro on app '%s': path must be a directory, got %s\n", appName, appPath)
+		}
+	}
+
+	return valid
 }
 
 func main() {
 	args := os.Args
 
-	if len(args) <= 1  {
+	if len(args) <= 1 {
 		return
 	}
 
-        appListJson, err := os.ReadFile("./apps.json")
+	appListJson, err := os.ReadFile("./apps.json")
 
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "Failed to read app list: %s\n", err)
-        } else {
-            KnownApps = parseAppList(appListJson)
-        }
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to read app list: %s\n", err)
+	} else {
+		KnownApps = parseAppList(appListJson)
+	}
 
 	script := "export PATH=${PATH}"
-        var newEntries []string
+	var newEntries []string
 
-        switch args[1] {
-        case "check":
-            IsConfigValid()
-            return;
+	switch args[1] {
+	case "check":
+		IsConfigValid()
+		return
 
-        case "add":
-            newEntries = loadPaths(args[2:])
+	case "add":
+		newEntries = loadPaths(args[2:])
 
-        default:
-            newEntries = args[1:]
-        }
-        separator := string(os.PathListSeparator)
-        script += separator + strings.Join(newEntries, separator)
+	default:
+		newEntries = args[1:]
+	}
+    
+	separator := string(os.PathListSeparator)
+	script += separator + strings.Join(newEntries, separator)
 
-        fmt.Println(script)
+	fmt.Println(script)
 }
